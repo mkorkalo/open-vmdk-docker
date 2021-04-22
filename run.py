b80ac53e205c3a69cc39b79810d9310106fb87e6
Mikko Korkalo <mikko.korkalo@dynave.fi>
2021-04-22 16:37:46 +0300

    add support for qemu-img vmdk conversion

--- run.py
+++ run.py
@@ -10,6 +10,8 @@ DOCKER_OVA_OUTPUTDIR = "/out/ova_outputdir"
 DOCKER_VMDK_IMAGE_DIR = "/out/vmdk_image_dir"
 
 parser = argparse.ArgumentParser(description="Process some integers.")
+parser.add_argument("-q", "--no-qemu-convert", action="store_true", default=False)
+parser.add_argument("-Q", "--qemu-convert-options", type=str, default="adapter_type=lsilogic,subformat=streamOptimized,compat6")
 parser.add_argument("--ova_name", "-n", type=str, default="my-vm")
 parser.add_argument(
     "--ovf_template", "-t", type=str, default="./template-hw14-bios.ovf"
@@ -18,6 +20,10 @@ parser.add_argument("--ova_outputdir", "-o", type=str, default=".")
 parser.add_argument("--vmdk_image", "-i", nargs="+", type=str, required=True)
 args = parser.parse_args()
 
+qconv = not args.no_qemu_convert
+print(f"qemu convert: {not args.no_qemu_convert}")
+if qconv:
+    print(f"qemu convert options: {args.qemu_convert_options}")
 print(f"OVA name: {args.ova_name}")
 print(f"OVF template: {args.ovf_template}")
 print(f"OVA output directory: {args.ova_outputdir}")
@@ -46,6 +52,21 @@ for vmdk_image in args.vmdk_image:
         if vmdk_image_dir_p != vmdk_image_p.parent:
             raise RuntimeError("VMDK images must be in the same directory")
 
+def convert_image(vmdk, vmdk_new, opts):
+    q_args = ["qemu-img", "convert", "-f", "vmdk", "-O", "vmdk", vmdk, vmdk_new, "-o", opts]
+    print("qemu convert command:", q_args)
+    subprocess.check_call(q_args)
+
+if qconv:
+    print("Performing vmdk image conversions:")
+    images = []
+    for vmdk_image in args.vmdk_image:
+        convert_image(vmdk_image, vmdk_image + ".converted.vmdk", args.qemu_convert_options)
+        images.append(vmdk_image + ".converted.vmdk")
+else:
+    print("Skip VMDK conversion")
+    images = args.vmdk_image
+
 cmd = ["docker", "run", "-w", DOCKER_OVA_OUTPUTDIR]
 
 mounts = [
@@ -66,7 +87,7 @@ cmd.append("mkova.sh")
 
 cmd.append(args.ova_name)
 cmd.append(str(Path(DOCKER_OVF_DIR) / ovf_template_p.name))
-for vmdk_image in args.vmdk_image:
+for vmdk_image in images:
     cmd.append(str(Path(DOCKER_VMDK_IMAGE_DIR) / Path(vmdk_image).name))
 
 print("Running:")
